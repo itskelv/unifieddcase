3e47081e5187be86b2c236470261cc3a9a474fb7
itskelv <kelvinsusantogunawan@gmail.com>
2025-11-24 14:30:42 +0800

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -15,11 +15,11 @@ def get_params(argv='1'):
 
         # INPUT PATH
         # dataset_dir='DCASE2020_SELD_dataset/',  # Base folder containing the foa/mic and metadata folders
-        dataset_dir='../DCASE2024_SELD_dataset/',
+        dataset_dir='../DCASE_FOA_dataset/',
 
         # OUTPUT PATHS
         # feat_label_dir='DCASE2020_SELD_dataset/feat_label_hnet/',  # Directory to dump extracted features and labels
-        feat_label_dir='../DCASE2024_SELD_dataset/seld_feat_label/',
+        feat_label_dir='FOA_features/',
 
         model_dir='models',  # Dumps the trained models and training curves in this folder
         dcase_output_dir='results',  # recording-wise results are dumped in this path.
